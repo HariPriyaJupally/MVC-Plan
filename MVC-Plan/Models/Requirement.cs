3aa9fdbc286ca27b632069b5fbb036360906eb64
sushmitha <kotyadasushmithad@gmail.com>
2019-03-03 16:07:27 -0600

    added the models requirement and StudentTerm.cs

---
+++ MVC-Plan/Models/Requirement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Plan.Models
+{
+    public class Requirement
+    {
+
+        public int RequirementID { get; set; }
+
+        public string  RequirementAbbrev { get; set; }
+        
+        public string RequirementName { get; set; }          
+        
+    }
+}
---
+++ MVC-Plan/Models/StudentTerm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_Plan.Models
+{
+    public class StudentTerm
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public int StudentTermId { get; set; }
+        [Required]
+        public int StudentID { get; set; }
+        [Required]
+        public int TermID { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public String TermName { get; set; }
+        [Required]
+        [MaxLength(10)]
+       
+        public ICollection<Student> Students { get; set; }
+    }
+}
